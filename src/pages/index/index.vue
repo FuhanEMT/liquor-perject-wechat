5cd8a6df3878d0f40334fb89fe0a8e2b197175b0
panda.li <625165799@qq.com>
2025-09-01 14:41:21 +0800

    fix: init

---
+++ src/pages/index/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <view class="index">
+    <nut-button type="primary" @click="onClick">按钮</nut-button>
+  </view>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+const show = ref(false)
+const onClick = () => {
+  show.value = true
+}
+</script>
